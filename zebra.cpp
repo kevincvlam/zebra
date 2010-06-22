e54b8039e29e4f13fe2479b3f6d1508db1ec5195
Kevin Lam <i.am.kev@gmail.com>
2010-06-22 19:37:47 -0400

    Instantiated Table, and printFn

--- zebra.cpp
+++ zebra.cpp
@@ -4,8 +4,48 @@
 #include <vector>
 using namespace std;
 
+void printTable(vector<int> t[5][5])
+{
+    int d,e,f;
+    for(d = 0; d < 5; d++)
+    {
+        for(e=0; e<5; e++)
+        {
+            for(f=0; f < t[d][e].size(); f++)
+            {
+                printf("%d", t[d][e][f]);
+            }
+            printf(" | ");
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
+    //instantiate the table w all possibilities
+    vector<int> table[5][5];
+    /* Legend:
+    1  2  3  4  5
+    Y  B  R  I  G
+    N  U  E  S  J
+    W  T  M  O  C
+    K  C  O  L  P
+    F  H  S  D  Z
+    */
+    int a,b,c;
+    for(int a = 0; a <5; a++)
+    {
+        for(int b=0; b<5; b++)
+        {
+            for(int c=1; c<6; c++)
+            {
+                table[a][b].push_back(c);
+            }
+        }
+    }
+
+    printTable(table);
 
     return 0;
 }
